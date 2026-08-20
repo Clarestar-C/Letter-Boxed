a0efb7411f4322af11954bfe5467983d06412bcf
Claresta <cheunc61@mcmaster.ca>
2026-08-20 15:48:47 -0400

    vite name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Letter-Boxed', // 👈 Replace with your exact repo name (case-sensitive)
 })
